a05c40222689d742bd1c6b2a9eff270fa39d4099
Hector Morales <tuntu99@gmail.com>
2021-12-14 10:00:25 -0600

    Band instance deleted because of error message

--- sockets/socket.js
+++ sockets/socket.js
@@ -3,12 +3,12 @@ const Bands = require('../models/bands');
 
 const bands = new Bands();
 
-bands.addBand(new Band('Queen'));
-bands.addBand(new Band('Led Zeppelin'));
-bands.addBand(new Band('Heroes del Silencio'));
-bands.addBand(new Band('Zoe'));
+// bands.addBand(new Band('Queen'));
+// bands.addBand(new Band('Led Zeppelin'));
+// bands.addBand(new Band('Heroes del Silencio'));
+// bands.addBand(new Band('Zoe'));
 
-console.log(bands);
+// console.log(bands);
 
 // console.log('init server');
 //Socket Messages
