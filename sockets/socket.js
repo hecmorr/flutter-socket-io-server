6e8e3ce31a09d5a549c3c4838ef35d3728e7381b
Hector Morales <tuntu99@gmail.com>
2022-02-04 21:06:36 -0600

    Final changes made in clearorder to upload the server to the repository

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,60 +1,54 @@
-const { io } = require('../index');
-const Bands = require('../models/bands');
-const Band = require('../models/band');
+const { io } = require("../index");
+const Bands = require("../models/bands");
+const Band = require("../models/band");
 
 const bands = new Bands();
-  
 
-bands.addBand(new Band('Queen'));
-bands.addBand(new Band('Led Zeppelin'));
-bands.addBand(new Band('Heroes del Silencio'));
-bands.addBand(new Band('Zoe'));
+//Temporal bands
+bands.addBand(new Band("Queen"));
+bands.addBand(new Band("Led Zeppelin"));
+bands.addBand(new Band("Heroes del Silencio"));
+bands.addBand(new Band("Zoe"));
 
 console.log(bands);
 
-// console.log('init server');
 //Socket Messages
-//Client represents a device connected to my socket service
-io.on('connection', client => {
-    console.log('Client connected');
-    client.emit('active-bands', bands.getBands());
-    //Callback that triggers once the client disconnects
-  client.on('disconnect', () => { 
-      console.log('Client disconnected');
+//Client represents a device connected to the socket service
+io.on("connection", (client) => {
+  console.log("Client connected");
+  client.emit("active-bands", bands.getBands());
+  //Callback that triggers once the client disconnects
+  client.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 
   //Listen event message
-  client.on('message', (payload)=>{
-      console.log('Mensaje!!', payload);
-    //Create event message
-    io.emit('message', {admin: 'New message'});
-      });
-  
-      client.on('emit-message', (payload)=>{
-        console.log(payload);
-        //This will emit the message to all clientes connected to the socket
-        //io.emit('new-message', payload);
-        //This one emits everybody except for the one who emitted originally
-        client.broadcast.emit('new-message', payload);
-      })
+  client.on("message", (payload) => {
+    console.log("Mensaje!!", payload);
+    //Creates event message
+    io.emit("message", { admin: "New message" });
+  });
 
-  client.on('vote-band', (payload)=>{
+  client.on("emit-message", (payload) => {
+    console.log(payload);
+
+    //This one emits everybody except for the one who emitted originally
+    client.broadcast.emit("new-message", payload);
+  });
+
+  client.on("vote-band", (payload) => {
     bands.voteBand(payload.id);
-    io.emit('active-bands', bands.getBands());
-  })
+    io.emit("active-bands", bands.getBands());
+  });
 
-  client.on('add-band', (payload)=>{
+  client.on("add-band", (payload) => {
     const newBand = new Band(payload.name);
     bands.addBand(newBand);
-    io.emit('active-bands', bands.getBands());
-  })
+    io.emit("active-bands", bands.getBands());
+  });
 
-  client.on('delete-band', (payload)=>{
-    
+  client.on("delete-band", (payload) => {
     bands.deleteBand(payload.id);
-    io.emit('active-bands', bands.getBands());
-  })
-
-
-     
+    io.emit("active-bands", bands.getBands());
+  });
 });
